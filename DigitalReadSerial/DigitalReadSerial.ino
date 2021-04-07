4660e1ac006b65adaabfccbe36bef946c6e155bb
apoleon33 <71031475+apoleon33@users.noreply.github.com>
2021-04-07 14:14:11 +0200

    Update DigitalReadSerial.ino

--- DigitalReadSerial/DigitalReadSerial.ino
+++ DigitalReadSerial/DigitalReadSerial.ino
@@ -1,7 +1,9 @@
 const int capt_1 = 14;
 const int capt_2 = 15;
-byte taux1;
-byte taux2;
+String trad1;
+String trad2;
+int taux1;
+int taux2;
 void setup(){
     Serial.begin(9600);  
     pinMode(capt_1,INPUT);
@@ -10,6 +12,8 @@ void setup(){
 void loop(){
     taux1=analogRead(capt_1);
     taux2=analogRead(capt_2);
-    Serial.println(String(taux1)+"a"+String(taux2)+"b");
+    Serial.println(taux1);
+    delay(1000);
+    Serial.println(taux2);
     delay(1000);
 }
