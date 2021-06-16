3779fdbf10b4ce6941d71819d8724101a8de9f2e
579 <simondumesny@gmail.com>
2021-06-16 13:15:40 +0200

    updated github action and the code

--- DigitalReadSerial/DigitalReadSerial.ino
+++ DigitalReadSerial/DigitalReadSerial.ino
@@ -1,18 +1,67 @@
-const int capt_1 = 14;
-const int capt_2 = 15;
+#include <dht.h>
+#define dht_apin A2
+dht DHT;
+const int capt_1 = A0;
+const int capt_2 = A1;
+const int pompe = 6;
+const int resistance = 3;
 String trad1;
 String trad2;
 int taux1;
 int taux2;
+int eau_hiver;
+int eau_été;
+int temperature_hiver;
+int temperature_été;
+int inputString;
+int junk;
+int a;
 void setup(){
     Serial.begin(9600);  
     pinMode(capt_1,INPUT);
     pinMode(capt_2,INPUT);
+    pinMode(pompe,OUTPUT);
+    pinMode(resistance,OUTPUT);
+    while (a!=1){
+        if(Serial.available()){ 
+            while(Serial.available()) { 
+                char inChar = (char)Serial.read(); //Lire l'entrée 
+                inputString += inChar; //Construit une chaine de caractére a partir des caractére reçus
+            } 
+            while (Serial.available() > 0) { 
+                junk = Serial.read() ; 
+            }
+            switch (inputString){
+                case 'A':
+                    eau_hiver='pot';
+                    eau_été='coupelle';
+                    temperature_hiver= 12;
+                    temperature_été= 25;
+                    a=1;
+                    break;
+                case 'B':
+                    eau_hiver='pot';
+                    eau_été='pot';
+                    temperature_hiver= 10;
+                    temperature_été= 25;
+                    a=1;
+                    break;
+                case 'C':
+                    eau_hiver='pot';
+                    eau_été='pot';
+                    temperature_hiver= 10;
+                    temperature_été= 25;
+                    a=1;
+                    break;
+            }
+            inputString = ""; 
+        }
+    }
+    delay(1000);
 }
 void loop(){
-    taux1=analogRead(capt_1);
-    taux2=analogRead(capt_2);
-    Serial.println(map(taux1,0,1024,0,100));
-    Serial.println(map(taux2,0,1024,0,100));
+    DHT.read11(dht_apin);
+    taux1=map(analogRead(capt_1),0,1024,0,100);
+    taux2=map(analogRead(capt_2),0,1024,0,100);
     delay(1000);
 }
