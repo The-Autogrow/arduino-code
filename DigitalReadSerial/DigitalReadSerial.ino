9dfd85857eca8742e7e9df6d48392d34e315fdbb
579 <simondumesny@gmail.com>
2021-06-18 20:23:50 +0200

    1st patch

--- DigitalReadSerial/DigitalReadSerial.ino
+++ DigitalReadSerial/DigitalReadSerial.ino
@@ -93,7 +93,7 @@ void loop(){
         }
         digitalWrite(pompe,LOW);
     }
-    Serial.println(DHT.temperature)
-    Serial.println(DHT.temperature)
+    Serial.println(DHT.temperature);
+    Serial.println(DHT.temperature);
     delay(1000);
 }
