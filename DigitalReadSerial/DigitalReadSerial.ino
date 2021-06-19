e5ec621d89aea331303d16962202cded2543cbef
579 <simondumesny@gmail.com>
2021-06-19 20:11:35 +0200

    changed dht11's library

--- DigitalReadSerial/DigitalReadSerial.ino
+++ DigitalReadSerial/DigitalReadSerial.ino
@@ -1,7 +1,7 @@
-#include <dht.h>
+#include <dht11.h>
 #include <RTClib.h>
-#define dht_apin A2
-dht DHT;
+#define dht_apin 4
+dht11 DHT11;
 RTC_DS3231 rtc;
 const int capt_1 = A0;
 const int capt_2 = A1;
@@ -10,13 +10,17 @@ const int resistance = 3;
 String trad1, trad2;
 int taux1,taux2,eau_hiver,eau_été,temperature_hiver,temperature_été,inputString,junk,a;
 void setup(){
-    Serial.begin(9600);  
+    Serial.begin(9600);
+    Serial.println("lets go");
     pinMode(capt_1,INPUT);
     pinMode(capt_2,INPUT);
     pinMode(pompe,OUTPUT);
     pinMode(resistance,OUTPUT);
     if (! rtc.begin()){
         digitalWrite(13, HIGH);
+        delay(1000);
+        digitalWrite(13,LOW);
+        Serial.println("ca marche pas");
         while (1);
    }
     while (a!=1){
@@ -35,6 +39,7 @@ void setup(){
                     temperature_hiver= 12;
                     temperature_été= 25;
                     a=1;
+                    Serial.println("yeah");
                     break;
                 case 'B':
                     eau_hiver='pot';
@@ -57,14 +62,14 @@ void setup(){
     delay(1000);
 }
 void loop(){
+    int chk = DHT11.read(dht_apin);
     DateTime time = rtc.now();
-    DHT.read11(dht_apin);
     taux1=map(analogRead(capt_1),0,1024,0,100);
     taux2=map(analogRead(capt_2),0,1024,0,100);
     if ( time.month() >= 4 and time.month() <= 9){//été
-        if (DHT.temperature < temperature_été){//gestion temperature
+        if (DHT11.temperature < temperature_été){//gestion temperature
             digitalWrite(resistance,HIGH);
-            while(DHT.temperature < temperature_été);
+            while(DHT11.temperature < temperature_été);
         }
         digitalWrite(resistance,LOW);
         if (taux1 < 20 and eau_été=='coupelle'){
@@ -78,9 +83,9 @@ void loop(){
         digitalWrite(pompe,LOW);
     } 
     else{//hiver
-        if (DHT.temperature < temperature_hiver){//gestion temperature
+        if (DHT11.temperature < temperature_hiver){//gestion temperature
             digitalWrite(resistance,HIGH);
-            while(DHT.temperature < temperature_hiver);
+            while(DHT11.temperature < temperature_hiver);
         }
         digitalWrite(resistance,LOW);
         if (taux1 < 20 and eau_hiver=='coupelle'){
@@ -93,7 +98,7 @@ void loop(){
         }
         digitalWrite(pompe,LOW);
     }
-    Serial.println(DHT.temperature);
-    Serial.println(DHT.temperature);
+    Serial.println((float)DHT11.humidity, 2);
+    Serial.println((float)DHT11.temperature, 2);
     delay(1000);
 }
